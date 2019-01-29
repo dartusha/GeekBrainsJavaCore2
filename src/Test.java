9001408f04f550ba43d9fe0eb9c49ec8a216b196
Maria Glazova <m_glazova@yahoo.com>
2019-01-29 10:40:52 +0300

    Изменение содержимого

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 public class Test {
     private int sum=0;
 
+    //testing
     public Test(){
       // String[][] arr={{"1","2","3","4"},{"test","test","5","6"}}; //тест кейс 1
         String[][] arr={{"1","2","3","4"},{"test","test","5","6"},{"7","8","9","0"},{"1","2","3","4"}}; //тест кейс 2
