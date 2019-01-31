cad483b39575d9dd18d219bf7bc48f95987ca576
Maria Glazova <glazova@bpcbt.com>
2019-01-31 21:26:13 +0300

    Добавлено решение задания 2, урок 3

--- src/Test.java
+++ src/Test.java
@@ -5,8 +5,20 @@ public class Test {
     и MyArrayDataException и вывести результат расчета.
      */
     public static void main(String[] args) {
-        Task1 task=new Task1();
+        System.out.println("Задание 1");
+        String[] arr={"на","дворе","трава","на", "траве","дрова","не","руби","дрова","на","траве","двора"};
+        Task1 task1=new Task1(arr);
+        System.out.println("");
 
+        System.out.println("Задание 2");
+        Task2 task2=new Task2();
+        task2.add("Иванов","1111111");
+        task2.add("Иванов","2222222");
+
+        task2.add("Петров","3333333");
+
+        task2.get("Иванов");
+        task2.get("Петров");
     }
 
 }
